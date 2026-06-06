3c97bd759caccbb682df802ef00b2b8e4ae265ea
Daniel Balparda <balparda@gmail.com>
2026-06-06 18:00:41 +0100

    version

--- src/tranzoom/__init__.py
+++ src/tranzoom/__init__.py
@@ -1,9 +1,9 @@
 # SPDX-FileCopyrightText: Copyright 2026 <balparda@github.com> & <BellaKeri@github.com>
 # SPDX-License-Identifier: Apache-2.0
-"""TranZoom: Fractal manipulation with LLMs."""
+"""TranZoom: Mandelbrot/Julia Fractal generation and manipulation with AI/LLMs."""
 
 __all__: list[str] = ['__author__', '__version__']
-__version__ = '1.8.0'  # also update pyproject.toml
+__version__ = '1.8.1'  # also update pyproject.toml
 __author__ = 'Daniel Balparda <balparda@github.com>, Bella Keri <BellaKeri@github.com>'
 
 __app__ = 'tranZoom'
